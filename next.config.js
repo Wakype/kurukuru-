465d76309aa27318adc9b1d2a34e2092f083bffb
Zul Fahmi <Zul@WOWBIDs-MacBook-Pro.local>
2023-05-29 16:47:31 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192, // or any other limit you desire
+            fallback: 'file-loader',
+            publicPath: '/_next/static/sounds',
+            outputPath: 'static/sounds',
+            name: '[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
